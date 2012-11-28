2f194d3e7ee92040aeb415352b5d9c7d94182dd9
Ola Spjuth <ola.spjuth@gmail.com>
2012-11-28 21:16:46 +0100

    added Entry model object

---
+++ plugins/net.bioclipse.icebear/src/net/bioclipse/icebear/business/Entry.java
@@ -0,0 +1,15 @@
+package net.bioclipse.icebear.business;
+public class Entry {
+
+	public String predicate;
+	public String object;
+	
+	public Entry(String predicate, String object) {
+		super();
+		this.predicate = predicate;
+		this.object = object;
+	}
+
+	
+
+}
